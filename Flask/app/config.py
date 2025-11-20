b101871db8c294d4cc41674307ccda1ae42a3d69
francisdurnez <francis.durnez@ugent.be>
2025-11-20 14:59:36 +0100

    francis solution

--- Flask/app/config.py
+++ Flask/app/config.py
@@ -1,4 +1,12 @@
+import os
+
 class Config:
     SECRET_KEY = 'a3b7c0f9c61a497e8f6d43b295de0f5dfd0a54e7d601e802d94ec32d58b99b8d'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Group_7@db.lkvstjspyeslqzzjmyca.supabase.co:5432/postgres'
+
+    # probeer eerst environment variable DATABASE_URL, anders gebruik SQLite lokaal
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'sqlite:///database.db'
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
